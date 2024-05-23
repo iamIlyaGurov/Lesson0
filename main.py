bb7bc43f0220c75fd7fce93f639f8bb24c3244ee
iamIlyaGurov <iam.gurov@yandex.ru>
2024-05-23 20:54:02 +0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,31 @@
+# Задача 1
+print(len('Hello'))
+print(len('UrbanAcademy'))
+print(len('5'))
+
+# Задача 2
+first = 5
+second = 10
+summa = first + second
+diff = first - second
+print(summa)
+print(diff)
+
+# Задача 3
+first = 4
+second = 9
+third = 6
+mean = (first + second + third) / 3
+print(mean)
+
+# Задача 4
+first_string = 'Вторник'
+second_string = 'Понедельник'
+print(second_string + ", " + first_string)
+
+# Задача 5
+a = 3
+b = 7
+c = 9
+f = ((a * b) + (a * c))
+print((f**3)/2)
